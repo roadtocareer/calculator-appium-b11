a880e0258ee10081c8ad5879f913b9728e18dc71
roadtocareer <salman@roadtocareer.net>
2024-07-31 22:14:19 +0600

    usage of @AfterMethod annotation for clear screen

--- src/test/java/CalcScreen.java
+++ src/test/java/CalcScreen.java
@@ -6,16 +6,16 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class CalcScreen {
-    @FindBy(id="com.google.android.calculator:id/digit_7")
-    WebElement btn7;
-    @FindBy(id="com.google.android.calculator:id/digit_5")
-    WebElement btn5;
     @FindBy(id="com.google.android.calculator:id/op_add")
     WebElement btnAdd;
+    @FindBy(id="com.google.android.calculator:id/op_sub")
+    WebElement btnSub;
     @FindBy(id="com.google.android.calculator:id/eq")
     WebElement btnEqual;
     @FindBy(id="com.google.android.calculator:id/result_final")
     WebElement txtResult;
+    @FindBy(id="com.google.android.calculator:id/clr")
+    WebElement btnClear;
     AndroidDriver driver;
 
     public CalcScreen(AndroidDriver driver){
@@ -25,6 +25,14 @@ public class CalcScreen {
     public int doSum(int num1, int num2){
         driver.findElement(By.id("com.google.android.calculator:id/digit_"+num1)).click();
         btnAdd.click();
+        driver.findElement(By.id("com.google.android.calculator:id/digit_"+num2)).click();
+        btnEqual.click();
+        return Integer.parseInt(txtResult.getText());
+
+    }
+    public int doSub(int num1, int num2){
+        driver.findElement(By.id("com.google.android.calculator:id/digit_"+num1)).click();
+        btnSub.click();
         driver.findElement(By.id("com.google.android.calculator:id/digit_"+num2)).click();
         btnEqual.click();
         return Integer.parseInt(txtResult.getText());
--- src/test/java/CalcTestRunner.java
+++ src/test/java/CalcTestRunner.java
@@ -1,12 +1,25 @@
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 public class CalcTestRunner extends Setup {
-    @Test
+    @Test(priority = 1, description = "Do sum")
     public void doSum(){
         CalcScreen calcScreen=new CalcScreen(driver);
         int res= calcScreen.doSum(8,6);
         System.out.println(res);
         Assert.assertEquals(res,14);
+    }
+    @Test(priority = 2, description = "Do sub")
+    public void doSub(){
+        CalcScreen calcScreen=new CalcScreen(driver);
+        int res= calcScreen.doSub(8,6);
+        System.out.println(res);
+        Assert.assertEquals(res,2);
+    }
+    @AfterMethod
+    public void clearScreen(){
+        CalcScreen calcScreen=new CalcScreen(driver);
+        calcScreen.btnClear.click();
     }
 }
